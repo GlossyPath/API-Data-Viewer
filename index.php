f5706f96e3a6169f4b5c199f1f6221010e3ac273
GlossyPath <glossypath0808@gmail.com>
2024-07-20 11:37:45 +0200

    codigo para obtener los datos de temperatura hecho

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-$apiKey = "ebecefa8d9fbd75c2ff749772e1f82af";
-
-$city = "Valencia";
+if (isset($_GET['Ciudad']) && !empty($_GET['Ciudad'])) {
+    $apiKey = "ebecefa8d9fbd75c2ff749772e1f82af";
+    $city = htmlspecialchars($_GET['Ciudad']);
 
 $url = "http://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$apiKey}&units=metric";
 
@@ -10,7 +10,17 @@ $ch = curl_init($url);
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-curl_close($ch); //cerramos curl
+$response = curl_exec($ch);
+
+if($response === false) {
+    $error = curl_error($ch);
+    echo "cURL error: $error";
+} else {
+    $data = json_decode($response, true);
+}
+
+curl_close($ch);
+}
 
 ?>
 
@@ -24,11 +34,19 @@ curl_close($ch); //cerramos curl
     <style>
 
         :root {
-            background-color: #F6FB7A;
+            background: linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%);;
         }
 
         body{
-            color: #73BBA3;
+            color: #FF0000;
+            font-weight: 900;
+            font-size: 1.5rem;
+        }
+
+        h1{
+            text-align: center;
+            text-transform: uppercase;
+
         }
 
     </style>
@@ -37,12 +55,13 @@ curl_close($ch); //cerramos curl
 <body>
 
 <h1>
-    <?= $city ?>
+    Consulta el Clima en Tu Ciudad
 </h1>
 
 <h2>
     Buscar el tiempo en la ciudad deseada
 </h2>
+
 <div class="formulario">
     <form action="index.php" method="get" >
         <p>Ciudad:
@@ -55,7 +74,12 @@ curl_close($ch); //cerramos curl
     </form>
 </div>
 
+<?php if (isset($data) && $data['cod'] === 200): ?>
+    <h2>Clima en <?= htmlspecialchars($city) ?></h2>
+    <p>La temperatura es de <?= $data['main']['temp'] ?>°C con <?= $data['weather'][0]['description'] ?>.</p>
+<?php elseif (isset($data['cod']) && $data['cod'] != 200): ?>
+    <p>Error al obtener el clima: <?= htmlspecialchars($data['message']) ?>.</p>
+<?php endif; ?>
 
 </body>
 </html>
-
