a6852b150d3eb27451b43690afed46855e4b8a55
GlossyPath <glossypath0808@gmail.com>
2024-07-24 19:31:21 +0200

    css + tipografia añadida

--- index.php
+++ index.php
@@ -34,6 +34,16 @@ curl_close($ch);
 
     <style>
 
+        @font-face{
+            font-family: "Lemon";
+            src: url(tipografia/Scratchy\ Lemon.ttf);
+        }
+
+        @font-face{
+            font-family: "Cheese";
+            src: url(tipografia/Keep\ Cheese.ttf)
+        }
+
         :root {
             background-color: #EEEEEE;
         }
@@ -41,14 +51,7 @@ curl_close($ch);
         html, body {
             height: 100%;
             margin: 0;
-        }
-
-        body{
-            color: #201E43;
-            font-weight: 900;
             font-size: 1.5rem;
-            display: flex;
-            flex-direction: column;
         }
 
         main{
@@ -60,10 +63,31 @@ curl_close($ch);
             text-align: center;
             text-transform: uppercase;
             background-color: #508C9B;
+            font-family: "Lemon";
+            padding: 10px;
         }
 
-        .resultado{
+        h2{
+            padding-left: 10px;
+        }
 
+        h3{
+            text-transform: uppercase;
+            background-color:#508C9B;
+            text-align: center;
+            padding: 3px;
+        }
+
+        body{
+            color: #201E43;
+            font-weight: 900;
+            display: flex;
+            flex-direction: column;
+            font-family: "Cheese";
+        }
+
+        .resultado p, .formulario {
+            padding-left: 10px;
         }
 
         footer p{
@@ -71,6 +95,7 @@ curl_close($ch);
             text-align: center;
             border-top: 3px solid #ddd;
             background-color: #508C9B;
+            padding: 3px;
         }
 
     </style>
@@ -78,41 +103,43 @@ curl_close($ch);
 
 
 <body>
-        <main>
-<h1>
-    Consulta el Clima
-</h1>
 
-<h2>
-    Introduce el nombre de la ciudad que quieres buscar
-</h2>
+    <main>
+        <h1>
+            Consulta el Clima
+        </h1>
 
-<div class="formulario">
-    <form action="index.php" method="get" >
-        <p>Ciudad:
-            <input type="text" name="Ciudad" size="40" required>
-        </p>
-        <p>
-            <input type="submit" value="Enviar">
-            <input type = "reset" value="Borrar">
-        </p>
-    </form>
-</div>
+        <h2>
+            Introduce el nombre de la ciudad que quieres buscar
+        </h2>
 
-<div class="resultado">
-    <?php if (isset($data) && $data['cod'] === 200): ?>
-        <h2>Clima en <?= htmlspecialchars($city) ?> es de:</h2>
-        <p>La temperatura es de <?= $data['main']['temp'] ?>°C con <?= $data['weather'][0]['description'] ?>.</p>
-        <p>La temperaura mínima es de <?= $data["main"]["temp_min"] ?>°C</p>
-        <p>La temperaura máxima es de <?= $data["main"]["temp_max"] ?>°C</p>
-        <p>La sensación térmica es de <?=$data["main"]["feels_like"] ?>°C</p>
-        <p>Velocidad del viento es de <?= $data["wind"]["speed"] ?> km/h </p>
-        <p>Porcentaje de humedad es de <?= $data["main"]["humidity"] ?> % </p>
-    <?php elseif (isset($data['cod']) && $data['cod'] != 200): ?>
-        <p>Error al obtener el clima: <?= htmlspecialchars($data['message']) ?>.</p>
-    <?php endif; ?>
-</div>
-        </main>
+        <div class="formulario">
+            <form action="index.php" method="get" >
+                <p>Ciudad:
+                    <input type="text" name="Ciudad" size="40" required>
+                </p>
+                <p>
+                    <input type="submit" value="Enviar">
+                    <input type = "reset" value="Borrar">
+                </p>
+            </form>
+        </div>
+
+        <div class="resultado">
+            <?php if (isset($data) && $data['cod'] === 200): ?>
+                <h3>Clima en <?= htmlspecialchars($city) ?> es de</h3>
+                <p>La temperatura es de <?= $data['main']['temp'] ?>°C con <?= $data['weather'][0]['description'] ?>.</p>
+                <p>La temperaura mínima es de <?= $data["main"]["temp_min"] ?>°C.</p>
+                <p>La temperaura máxima es de <?= $data["main"]["temp_max"] ?>°C.</p>
+                <p>La sensación térmica es de <?=$data["main"]["feels_like"] ?>°C.</p>
+                <p>Velocidad del viento es de <?= $data["wind"]["speed"] ?> km/h. </p>
+                <p>Porcentaje de humedad es de <?= $data["main"]["humidity"] ?> %.</p>
+            <?php elseif (isset($data['cod']) && $data['cod'] != 200): ?>
+                <p>Error al obtener el clima: <?= htmlspecialchars($data['message']) ?>.</p>
+            <?php endif; ?>
+        </div>
+    </main>
+
 <footer>
     <p>
         Desarrollado por GlossyPath
